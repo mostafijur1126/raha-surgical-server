fc3ec3b01aa14604a3cef40f051bbb8e478ee483
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-01 21:30:32 +0600

    create insert order api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { productsCollection } from "./config/db";
+import { ordersCollection, productsCollection } from "./config/db";
+import { ObjectId } from "mongodb";
 
 const app = express();
 
@@ -10,10 +11,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server Running...");
 });
-
+//add product
 app.post("/add-product", async (req, res) => {
   const paylod = {
     ...req.body,
+    featured: false,
     createdAt: new Date(),
     updatedAt: new Date(),
   };
@@ -26,7 +28,7 @@ app.post("/add-product", async (req, res) => {
     },
   });
 });
-
+//get all products
 app.get("/products", async (req, res) => {
   try {
     const { category } = req.query;
@@ -50,7 +52,29 @@ app.get("/products", async (req, res) => {
     });
   }
 });
-
+//get product by id
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productsCollection.findOne({ _id: new ObjectId(id) });
+    if (!product) {
+      return res.status(404).send({
+        succsess: false,
+        message: "Product not found",
+      });
+    }
+    res.send({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Failed to fetch products",
+    });
+  }
+});
+//get all categories
 app.get("/categories", async (req, res) => {
   try {
     const categories = await productsCollection.distinct("category");
@@ -62,6 +86,51 @@ app.get("/categories", async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Faild to fetch categories",
+    });
+  }
+});
+//get featured products
+app.get("/featured-products", async (req, res) => {
+  try {
+    const products = await productsCollection
+      .find({ featured: false })
+      .limit(8)
+      .toArray();
+
+    res.send({
+      success: true,
+      data: products,
+    });
+  } catch {
+    res.status(500).send({
+      success: false,
+      message: "Failed to fetch featured products",
+    });
+  }
+});
+
+//order product
+app.post("/order-product", async (req, res) => {
+  try {
+    const paylod = {
+      ...req.body,
+      status: "pending",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const orderedProduct = await ordersCollection.insertOne(paylod);
+    res.send({
+      success: true,
+      data: {
+        ...paylod,
+        _id: orderedProduct.insertedId,
+      },
+    });
+    // Implementation for ordering product
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Failed to order product",
     });
   }
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,7 @@ const client = new MongoClient(process.env.MONGODB_URI!);
 
 export const db = client.db("raha-surgical-db");
 export const productsCollection = db.collection("products");
+export const ordersCollection = db.collection("orders");
 
 export async function connectDB() {
   await client.connect();
